d2b5f6ad16feab97444eb9fa469e99ee0c65d571
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-03-28 06:14:13 -0400

    outlined pseudocode for script.js file

---
+++ script.js
@@ -0,0 +1,8 @@
+// user enters a value into the form
+
+// math is performed on the value to convert c into f
+// multiply by 1.8 (or 9/5) and add 32
+
+// both numbers are printed on the page
+
+// reset button to refresh the page and remove the values
