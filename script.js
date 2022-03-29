5ade425589fed30d45b23348b1c3a774334c7cce
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-03-29 02:30:37 -0400

    updated script.js; functions working

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+$("#results").attr("hidden", true)
+
 // user enters a value into the form
+$("#convertBtn").on("click", function(event) {
+    event.preventDefault()
+    convertValue()
+})
 
-// math is performed on the value to convert c into f
-// multiply by 1.8 (or 9/5) and add 32
+function convertValue() {
+    // math is performed on the value to convert c into f
+    // multiply by 1.8 and add 32
+    let base = $("#cTemp").val()
+    let newValue = Math.round(base * 1.8 + 32)
 
-// both numbers are printed on the page
+    // both numbers are printed on the page
+    $("#results").attr("hidden", false)
+    $("#base").append(base)
+    $("#newValue").append(newValue)
+}
 
 // reset button to refresh the page and remove the values
+$("#resetBtn").on("click", function(event) {
+    window.location.reload()
+})
