fcb06b22ecb6a42995edf07fc13c2adbd61f1cb1
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-03-29 03:18:21 -0400

    added instructions in header; added functionality to convert from F to C

--- script.js
+++ script.js
@@ -7,15 +7,30 @@ $("#convertBtn").on("click", function(event) {
 })
 
 function convertValue() {
-    // math is performed on the value to convert c into f
-    // multiply by 1.8 and add 32
-    let base = $("#cTemp").val()
-    let newValue = Math.round(base * 1.8 + 32)
+    // if user enters C, convert to F
+    if ($("#cTemp").val() != "") {
+        // multiply by 1.8 and add 32
+        let cBase = $("#cTemp").val()
+        let newCValue = Math.round(cBase * 1.8 + 32)
 
-    // both numbers are printed on the page
-    $("#results").attr("hidden", false)
-    $("#base").append(base)
-    $("#newValue").append(newValue)
+        // both numbers are printed on the page
+        $("#results").attr("hidden", false)
+        $("#base").append(cBase + "&deg;C")
+        $("#newValue").append(newCValue + "&deg;F")
+    } else if ($("#fTemp").val() != "") {
+        // if user enters F, convert to C
+        // divide by 1.8 subtract add 32
+        let fBase = $("#fTemp").val()
+        let newFValue = Math.round((fBase - 32) / 1.8)
+
+        // both numbers are printed on the page
+        $("#results").attr("hidden", false)
+        $("#base").append(fBase + "&deg;F")
+        $("#newValue").append(newFValue + "&deg;C")
+    } else {
+        $("#results").attr("hidden", false)
+        $("#results").append()
+    }
 }
 
 // reset button to refresh the page and remove the values
